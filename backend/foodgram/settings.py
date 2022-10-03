22bdee78534f2b522bf471bcea38ff61662af996
Anastasia Amosova <anastasia.am0s0va@yandex.ru>
2022-10-03 12:09:03 +0300

    Deploy ststic

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -101,12 +101,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-STATIC_ROOT = os.path.join(BASE_DIR, '/app_data/static/')
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
