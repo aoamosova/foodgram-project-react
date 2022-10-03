045cb0cd65d50b4ab249af23fe232db712406522
Anastasia Amosova <anastasia.am0s0va@yandex.ru>
2022-10-03 14:30:44 +0300

    deploy urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,6 +8,6 @@ router = routers.DefaultRouter()
 router.register('users', CustomUsersViewSet, basename='users')
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("", include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
