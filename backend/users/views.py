82812a8688b34c3a21e496eedfeb8a70b5c93cad
Anastasia Amosova <anastasia.am0s0va@yandex.ru>
2022-10-03 19:35:15 +0300

    Flake8

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from users.models import Subscriptions, User
 from users.serializers import CustomUserSerializer, SubscriptionSerializer
